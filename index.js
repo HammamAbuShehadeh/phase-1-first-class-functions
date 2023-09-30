50e0582e2a56192a8247750d518ec0c335bce42a
Hammam AbuShehadeh <hammam.y.asmar@gmail.com>
2023-09-30 20:54:47 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const receivesAFunction = (callback) => {
+  callback();
+};
+const returnsANamedFunction = () => {
+  return function named() {};
+};
+const returnsAnAnonymousFunction = () => {
+  return function () {};
+};
